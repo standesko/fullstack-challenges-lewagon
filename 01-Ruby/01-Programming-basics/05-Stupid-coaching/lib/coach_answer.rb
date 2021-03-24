c7ccf90390a04af370a5f26492c1e56c6e4a61d7
stan de sko <stanislas.deskowronski@gmail.com>
2021-03-24 16:41:43 +0100

    Stupid Coaching 2

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -11,15 +11,14 @@ end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
-  if your_message.downcase == "i am going to work right now!"
-    ""
-  elsif your_message.end_with?("?")
-    "Silly question, get dressed and go to work!"
-  elsif your_message.upcase
-    "I can feel your motivation! I don't care, get dressed and go to work!"
-  elsif your_message.end_with?("?").upcase
-    "I can feel your motivation! Silly question, get dressed and go to work!"
+  answer = coach_answer(your_message)
+  if answer != ""
+    if your_message.upcase == your_message
+      "I can feel your motivation! #{answer}"
+    else
+      answer
+    end
   else
-    "I don't care, get dressed and go to work!"
+    ""
   end
 end
